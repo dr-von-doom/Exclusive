189075d0b093019300d147846948a7577916a394
Sonya Castro <sonya.castro.gomez@gmail.com>
2024-09-17 22:33:54 -0500

    update layouts

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -3,9 +3,12 @@ import BaseLayout from "@/layouts/BaseLayout";
 const NotFoundPage = () => {
   return (
     <BaseLayout>
-      <div>
-        <h1>404</h1>
-        <p>Page not found</p>
+      <div className="flex h-full grow flex-col items-center justify-center bg-slate-100 p-10">
+        <h1>Not found</h1>
+        <p>
+          The page you are looking for does not exist. Please check the URL in
+          the address bar and try again.
+        </p>
       </div>
     </BaseLayout>
   );
