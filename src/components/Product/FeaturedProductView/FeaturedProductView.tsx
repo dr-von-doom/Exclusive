d96419b306b2fd734ac6d32bf2ea2f8836777e89
Daniel Olivares <daolivaresrodelo@gmail.com>
2024-09-19 13:33:01 -0500

    Service eliminated

--- src/components/Product/FeaturedProductView/FeaturedProductView.tsx
+++ src/components/Product/FeaturedProductView/FeaturedProductView.tsx
@@ -1,26 +1,13 @@
-import { useEffect, useState } from 'react';
-import { getFeaturedProducts } from "@/services/featured-product-service";
+import { FeaturedProducts } from "@/data/FeaturedProducts"
 import { Product } from "@/types/Product"
 import ProductCard from "../ProductCard"
 
 export const FeaturedProductView = () => {
-  const [products, setProducts] = useState<Product[]>([]);
-
-  useEffect(() => {
-    // Obtener los productos cuando el componente se monta
-    const fetchProducts = async () => {
-      const featuredProducts = getFeaturedProducts();
-      setProducts(featuredProducts);
-    };
-
-    fetchProducts();
-  }, []);
-
   return (
     <div className="px-4">
       <h2 className="text-xl md:text-2xl font-bold">Featured Products</h2>
       <div id="featured-product-container" className="grid grid-cols-[repeat(auto-fit,_minmax(300px,_1fr))] gap-6 mx-auto">
-          {products.map((product: Product) => (
+          {FeaturedProducts.map((product: Product) => (
           <ProductCard key={product.id} {...product} />
         ))}
       </div>
--- src/services/featured-product-service.ts
+++
@@ -1,7 +0,0 @@
-// services/productService.ts
-import { Product } from '../types/Product';
-import { FeaturedProducts } from '../data/FeaturedProducts';
-
-export const getFeaturedProducts = (): Product[] => {
-  return FeaturedProducts;
-};
