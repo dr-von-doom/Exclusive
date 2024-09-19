0479f6e09c3080d5c136021cc8e11c56c63e7a4f
Daniel Olivares <daolivaresrodelo@gmail.com>
2024-09-19 12:06:51 -0500

    Services implemented

--- src/components/Product/FeaturedProductView/FeaturedProductView.tsx
+++ src/components/Product/FeaturedProductView/FeaturedProductView.tsx
@@ -1,13 +1,26 @@
-import { FeaturedProducts } from "@/data/FeaturedProducts"
+import { useEffect, useState } from 'react';
+import { getFeaturedProducts } from "@/services/featured-product-service";
 import { Product } from "@/types/Product"
 import ProductCard from "../ProductCard"
 
 export const FeaturedProductView = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    // Obtener los productos cuando el componente se monta
+    const fetchProducts = async () => {
+      const featuredProducts = getFeaturedProducts();
+      setProducts(featuredProducts);
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <div className="px-4">
       <h2 className="text-xl md:text-2xl font-bold">Featured Products</h2>
       <div id="featured-product-container" className="grid grid-cols-[repeat(auto-fit,_minmax(300px,_1fr))] gap-6 mx-auto">
-          {FeaturedProducts.map((product: Product) => (
+          {products.map((product: Product) => (
           <ProductCard key={product.id} {...product} />
         ))}
       </div>
