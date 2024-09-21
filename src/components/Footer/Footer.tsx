381c028c82103af6b5b8c477a1fb266b7c20564b
EmilyRoldan <eroldan@uninorte.edu.co>
2024-09-21 10:26:26 -0500

    feat(HU2008): adding fonts

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,31 +1,31 @@
-import React from "react"; 
 import FacebookIcon from "@/assets/icons/facebook.svg";
 import InstagramIcon from "@/assets/icons/instagram.svg";
 import MastercardIcon from "@/assets/icons/mastercard.svg";
 import PaypalIcon from "@/assets/icons/paypal.svg";
 import VisaIcon from "@/assets/icons/visa.svg";
 import xIcon from "@/assets/icons/x.svg";
-import categories from '@/data/category-groups.json';
+import categories from "@/data/category-groups.json";
+import React from "react";
 
 export type Category = {
   id: number;
   name: string;
-}
+};
 
 export const Footer = () => {
-  const handleSubmit = (event:React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
   };
 
   return (
-    <footer className="font-poppins bg-black p-10 text-sm text-white">
+    <footer className="bg-black p-10 font-poppins text-sm text-white">
       <div className="mb-5 flex flex-col items-center justify-center text-center lg:flex-row lg:justify-between">
         <div className="mb-5 lg:mb-0">
           <p className="pl-0 lg:pl-10">Exclusive S.A.</p>
           <p className="pl-0 lg:pl-10">NIT: 123456789-0</p>
         </div>
 
-        <div className="mb-5 flex items-center gap-8 lg:mb-0">
+        <div className="mb-5 flex items-center gap-9 lg:mb-0">
           <a href="#">
             <img className="h-7 w-auto" src={FacebookIcon} alt="facebook" />
           </a>
@@ -52,10 +52,13 @@ export const Footer = () => {
 
       <div className="flex flex-col justify-between gap-7 bg-black p-10 text-center text-white lg:flex-row lg:text-left">
         <div>
-          <h3 className="mb-2 text-lg font-medium">Exclusive</h3>
-          <p>Subscribe</p>
-          <p>Get 10% off your first order</p>
-          <form onSubmit={handleSubmit} className="flex flex-col items-center lg:flex-row lg:items-start">
+          <h3 className="font-inter mb-2 text-lg font-medium">Exclusive</h3>
+          <p className="mb-1">Subscribe</p>
+          <p className="mb-1">Get 10% off your first order</p>
+          <form
+            onSubmit={handleSubmit}
+            className="mt-2 flex flex-col items-center lg:flex-row lg:items-start"
+          >
             <input
               type="email"
               placeholder="Enter your email"
@@ -71,9 +74,9 @@ export const Footer = () => {
         </div>
 
         <div>
-          <h3 className="mb-2 font-inter text-lg font-medium">Support</h3>
+          <h3 className="font-inter mb-2 text-lg font-medium">Support</h3>
           <p>111 Bijoy Sarani, Dhaka, Bangladesh.</p>
-          <p>
+          <p className="mt-1">
             <a
               href="mailto:info@exclusive.com"
               className="text-gray-400 hover:text-white"
@@ -81,7 +84,7 @@ export const Footer = () => {
               info@exclusive.com
             </a>
           </p>
-          <p>
+          <p className="mt-1">
             <a
               href="tel:+8801788888999"
               className="text-gray-400 hover:text-white"
@@ -92,7 +95,7 @@ export const Footer = () => {
         </div>
 
         <div>
-          <h3 className="mb-2 text-lg font-medium font-inter">Categories</h3>
+          <h3 className="font-inter mb-2 text-lg font-medium">Categories</h3>
           <ul className="list-none p-0">
             {categories.map((category: Category) => (
               <li key={category.id} className="mb-1">
@@ -104,9 +107,8 @@ export const Footer = () => {
           </ul>
         </div>
 
-
         <div>
-          <h3 className="mb-2 font-inter text-lg font-medium">Quick Links</h3>
+          <h3 className="font-inter mb-2 text-lg font-medium">Quick Links</h3>
           <ul className="list-none p-0">
             <li className="mb-1">
               <a href="#" className="text-gray-400 hover:text-white">
