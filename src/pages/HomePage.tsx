cb7475935a89b1ab18a7bc19fe02b26decfead8f
EmilyRoldan <eroldan@uninorte.edu.co>
2024-09-21 10:02:40 -0500

    feat(HU2014): finished

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,4 @@
 import BaseLayout from "@/layouts/BaseLayout";
-import Filter from "@/components/Filter";
 
 const HomePage = () => {
   return (
@@ -7,7 +6,6 @@ const HomePage = () => {
       <div className="flex h-full grow items-center justify-center bg-slate-100 p-10">
         <h1>Home Page</h1>
       </div>
-      <Filter /> 
     </BaseLayout>
   );
 };
