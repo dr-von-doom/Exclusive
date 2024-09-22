a334c676c0b29b0cb99ef1bd8533055092a6482d
Darío Mejía <72144279+DarioMejia@users.noreply.github.com>
2024-09-21 21:08:39 -0500

    fix(HU2012): fix mobile and some issues with padding

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BannerProps } from '@/types/Banner';
+import React from "react";
+import { BannerProps } from "@/types/Banner";
 
 const Banner: React.FC<BannerProps> = ({
   mainText,
@@ -11,26 +11,26 @@ const Banner: React.FC<BannerProps> = ({
 }) => {
   return (
     <section>
-      <div className="bg-black flex flex-col items-center rounded-[20px] m-4 p-2">
-        <p className="text-white font-light text-xxl tracking-[0.3em] m-2.5">
+      <div className="m-4 flex flex-col items-center rounded-[20px] bg-black p-6">
+        <p className="text-xxl mx-2.5 mt-2.5 mb-5 text-center font-light tracking-[0.3em] text-white">
           {mainText}
         </p>
 
-        <div className="flex flex-col md:flex-row items-center justify-around w-full">
-          <div className="flex flex-col items-center md:items-start justify-start">
-            <p className="font-semibold text-4xl tracking-[0.2em] text-green-500 font-sans mb-2.5 text-center md:text-left">
+        <div className="flex w-full flex-col items-center justify-around md:flex-row">
+          <div className="flex flex-col items-center justify-start md:items-start">
+            <p className="mb-5 text-center font-sans text-4xl font-semibold tracking-[0.2em] text-green-500 md:text-left">
               {title}
               <br />
               {subtitle}
             </p>
-            <div className="flex flex-row items-center gap-[30px]">
+            <div className="my-3 flex flex-row items-center gap-[30px]">
               {icons.map((icon, index) => (
                 <a
                   href={icon.href}
                   key={index}
-                  className="hover:scale-110 transition-transform duration-300 ease-in-out"
+                  className="transition-transform duration-300 ease-in-out hover:scale-110"
                 >
-                  <div className="w-[30px] h-[30px] text-white fill-current">
+                  <div className="h-[30px] w-[30px] fill-current text-white">
                     {icon.svg}
                   </div>
                 </a>
@@ -38,21 +38,21 @@ const Banner: React.FC<BannerProps> = ({
             </div>
           </div>
 
-          <div className="mt-4 md:mt-0 md:ml-4">
+          <div className="mt-4 md:ml-4 md:mt-0">
             <img
               src={imageSrc}
               alt="Products sale"
-              className="w-[250px] h-[200px]"
+              className="h-[200px] w-[250px]"
             />
           </div>
         </div>
 
-        <div className="flex flex-row items-center mt-4">
+        <div className="mt-4 flex flex-row items-center">
           <a
             href={shopNowLink}
-            className="text-white font-light text-xl tracking-[0.3em] m-2.5 border border-white rounded-md px-4 py-2 hover:bg-white hover:text-black transition-colors duration-300 ease-in-out"
+            className="m-2.5 rounded-md border border-white px-4 py-2 text-center text-xl font-light tracking-[0.3em] text-white transition-transform duration-300 ease-in-out hover:scale-110 hover:bg-white hover:text-black"
           >
-            Shop Now
+            <span className= "pl-[0.3em]">Shop Now</span>
           </a>
         </div>
       </div>
