e7a5cf857d19985fc91e7f3c8991bf5bb39b1a71
Daniel Olivares <daolivaresrodelo@gmail.com>
2024-09-22 17:03:36 -0500

    Feature completed with changes included

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,11 @@
 import BaseLayout from "@/layouts/BaseLayout";
 import { FeaturedProductView } from "@/components/Product/FeaturedProductView/FeaturedProductView";
 
+
 const HomePage = () => {
   return (
     <BaseLayout>
-      <div className=" bg-slate-100 p-10">
+      <div className="bg-slate-100 p-10">
         <FeaturedProductView />
       </div>
     </BaseLayout>
