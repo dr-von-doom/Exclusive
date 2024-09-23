978d606d09827040ae4a95f3e57543302699f0f9
Daniel Olivares <daolivaresrodelo@gmail.com>
2024-09-22 21:53:07 -0500

    Feature product list corrections

--- src/components/Product/FeaturedProductView/FeaturedProductView.tsx
+++ src/components/Product/FeaturedProductView/FeaturedProductView.tsx
@@ -8,11 +8,11 @@ export const FeaturedProductView = () => {
     .slice(0, 12); 
 
   return (
-    <div className="px-12">
-      <h2 className="text-xl font-bold mb-4 md:text-2xl">Featured Products</h2>
+    <div>
+      <h2 className="text-center text-xl font-bold mb-4 md:text-4xl">Featured Products</h2>
       <div
         id="featured-product-container"
-        className="mx-auto grid grid-cols-[repeat(auto-fit,_minmax(300px,_1fr))] gap-8">
+        className="grid grid-cols-[repeat(auto-fit,300px)] gap-8 justify-center mb-8">
         {topRatedProducts.map((product) => (
           <ProductCard key={product.id} {...product as Product} />
         ))}
