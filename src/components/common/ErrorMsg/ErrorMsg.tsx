ac07fc71e137ff4bbdb95f2f382145997ef5dc09
Sonya Castro <sonya.castro.gomez@gmail.com>
2024-09-28 21:26:41 -0500

    Create error component

---
+++ src/components/common/ErrorMsg/ErrorMsg.tsx
@@ -0,0 +1,21 @@
+export type ErrorMsgProps = {
+  title: string;
+  message: string;
+  imageUrl?: string;
+};
+
+export const ErrorMsg = ({
+  title = "Something went wrong...",
+  message,
+  imageUrl,
+}: ErrorMsgProps) => {
+  return (
+    <div className="mx-auto flex h-full max-w-screen-md flex-col items-center justify-center gap-3 p-10">
+      <h1 className="text-lg font-bold">{title}</h1>
+      <p>{message}</p>
+      {imageUrl && <img src={imageUrl} alt="Error" />}
+    </div>
+  );
+};
+
+export default ErrorMsg;
