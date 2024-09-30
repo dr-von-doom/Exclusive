b9a0c64ebfba9231e68153432db455e070d5ebe3
EmilyRoldan <eroldan@uninorte.edu.co>
2024-09-30 11:05:30 -0500

    feat(HU2017): fix skeleton

--- src/components/Home/PromotionalImage/PromotionalImagesSkeleton.tsx
+++ src/components/Home/PromotionalImage/PromotionalImagesSkeleton.tsx
@@ -2,8 +2,12 @@ import PlaceholderImage from "@/assets/icons/placeholder.svg?react";
 
 export const PromotionalImageSkeleton = () => {
   return (
-    <div className="flex flex-col justify-center items-center p-4 animate-pulse"> {/* Cambiado a flex-col */}
-      <PlaceholderImage className="h-40 w-full max-w-md" />
+    <div className="flex flex-col items-center justify-center">
+      <div className="h-40 w-full max-w-md animate-pulse overflow-hidden rounded-lg bg-gray-200 p-4 shadow-lg">
+        <PlaceholderImage className="h-full w-full" />
+      </div>
+      <div className="mt-4 h-4 w-3/4 rounded-full bg-gray-200"></div>
+      <div className="mt-2 h-4 w-1/2 rounded-full bg-gray-200"></div>
     </div>
   );
 };
